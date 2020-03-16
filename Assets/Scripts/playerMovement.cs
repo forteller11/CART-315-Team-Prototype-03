593cd4ce2d61320b7f3b592ccb4ce6e542a7fe84
mortebouse <31903560+mortebouse@users.noreply.github.com>
2020-03-16 19:02:38 -0400

    added in platformer player movement and collisions from my proto 3 (stuff needs to be modified for sure but at least its in there now)

---
+++ Assets/Scripts/playerMovement.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerMovement : MonoBehaviour
+{
+    public float movement_scaler;
+    public float jump_scaler;
+    public float max_speed;
+    float x_movement;
+
+    private Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = this.gameObject.GetComponent<Rigidbody2D>();
+        jump_scaler = 150;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        x_movement = Input.GetAxis("Horizontal");
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            Vector2 jump_force = new Vector2(0, jump_scaler);
+            rb.AddForce(jump_force);
+        }
+    }
+    void FixedUpdate()
+    {
+        if (rb.velocity.magnitude < max_speed)
+        {
+            Vector2 movement = new Vector2(x_movement, 0);
+            rb.AddForce(movement_scaler * movement);
+        }
+    }
+}
