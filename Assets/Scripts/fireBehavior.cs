46ca2d3884b3639d451c64767069e2cdc9e2a5a7
mortebouse <31903560+mortebouse@users.noreply.github.com>
2020-03-19 15:42:12 -0400

    added a flame that changes color according to which ability mode the player is in

---
+++ Assets/Scripts/fireBehavior.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class fireBehavior : MonoBehaviour
+{
+    private GameObject player;
+    playerMovement playerMovementScript;
+
+    Animator fireAnim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        fireAnim = GetComponent<Animator>();
+        player = GameObject.Find("PlayerSprite");
+        playerMovementScript = player.GetComponent<playerMovement>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        switch (playerMovementScript.abilityIndex)
+        {
+            case 0:
+                fireAnim.SetInteger("modeIndex", 0);
+                break;
+            case 1:
+                fireAnim.SetInteger("modeIndex", 1);
+                break;
+            case 2:
+                fireAnim.SetInteger("modeIndex", 2);
+                break;
+        }
+    }
+}
